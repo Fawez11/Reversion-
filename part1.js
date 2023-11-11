e5bec1756d7964eed2bc5e498fc13009c51f1815
Fawez11 <fawezfer12@gmail.com>
2023-11-11 09:48:22 +0100

    initial commit

---
+++ part1.js
@@ -0,0 +1,36 @@
+// JavaScript ES5 Basics Assessment
+// Question 1: Variables and Data Types
+
+// 1.1 Declare a variable called name and assign your name to it.
+
+// 1.2 Create a variable age and set it to your age.
+
+// 1.3 Define a constant variable PI and assign the value 3.14 to it.
+
+// 1.4 Create a variable isStudent and set it to true or false based on whether you are a student or not.
+
+// Question 2: Functions
+
+// 2.1 Write a function named add that takes two parameters a and b and returns their sum.
+
+// 2.2 Create a function called greet that takes a name parameter and returns a greeting message.
+
+// Question 3: Control Flow
+
+// 3.1 Write a for loop that prints numbers from 1 to 5.
+
+// 3.2 Create an array colors with three colors: red, green, and blue. Use a for loop to iterate over the array and log each color to the console.
+
+// Question 4: Objects and Arrays
+
+// 4.1 Create an object person with properties name, age, and isStudent. Populate these properties with your own information.
+
+// 4.2 Create an array fruits with at least three different fruits.
+
+// 4.3 Write a function called printFruits that takes an array of fruits as a parameter and prints each fruit to the console.
+
+// Question 5: Callbacks
+
+// 5.1 Write a function multiply that takes two parameters a and b and returns their product.
+
+// 5.2 Create a function calculate that takes three parameters: a, b, and a callback function. Call the callback function with a and b as arguments and return the result.
---
+++ part2.js
@@ -0,0 +1,17 @@
+// Question 6: Objects and Properties
+
+// 6.1 Extend the person object from Question 4 to include a new property called address. Provide a sample address with properties like street, city, and zipcode.
+
+// 6.2 Write a function called printPersonInfo that takes a person object as a parameter and logs their name, age, and address to the console.
+
+// Question 7: Arrays and Methods
+
+// 7.1 Add a new fruit to the fruits array from Question 4. Use an array method to remove one fruit from the beginning of the array.
+
+// 7.2 Write a function called hasFruit that takes a fruit name as a parameter and returns true if the fruit is in the array and false otherwise.
+
+// Question 8: Nested Objects and Arrays
+
+// 8.1 Create an object called book with properties title, author, and chapters. The chapters property should be an array containing the chapter titles.
+
+// 8.2 Write a function called printBookInfo that takes a book object as a parameter and logs its title, author, and the list of chapters to the console.
